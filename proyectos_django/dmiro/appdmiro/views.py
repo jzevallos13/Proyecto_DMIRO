cb479d32d2c98df81ec543b0b37a109bda819e84
jzevallos13 <37246131+jzevallos13@users.noreply.github.com>
2018-04-02 20:34:40 -0500

    Creacion de Views Para Agencias y Productos
    Creacion de View para enviar datos VIA JSON, con respecto a las Agencias y Productos

--- proyectos_django/dmiro/appdmiro/views.py
+++ proyectos_django/dmiro/appdmiro/views.py
@@ -32,4 +32,39 @@ def inicio_app(request):
 	else:
             return redirect('inicio')
 	return render(request,"system_inicio_general.html",context)
+	
+def inicio_app_detalles(request):
+    if request.user.is_authenticated() and not request.user.is_staff:
+        usuario = request.user
+        context = {
+            "usuario": usuario,
+        }
+    else:
+            return redirect('inicio')
+    return render(request,"system_detalles.html",context)
+
+def mapAgencias(request):
+    queryset = agencias.objects.all().order_by("id")
+    datasource = {}
+    datasource ['agencias'] = [] 
+    for agencia in queryset:
+        data = {}
+        data['id'] = agencia.id
+        data['age_nombre'] = agencia.age_nombre
+        data['age_direccion'] = agencia.age_direccion
+        data['age_coordenadax'] = agencia.age_coordenadax
+        data['age_coordenaday'] = agencia.age_coordenaday
+        datasource ['agencias'].append(data)
+    return JsonResponse(datasource)
+
+def todoProductos(request):
+    queryset = productos.objects.all().order_by("id")
+    datasource = {}
+    datasource ['productos'] = [] 
+    for producto in queryset:
+        data = {}
+        data['id'] = producto.id
+        data['pro_nombre'] = producto.pro_nombre
+        datasource ['productos'].append(data)
+    return JsonResponse(datasource)
 
