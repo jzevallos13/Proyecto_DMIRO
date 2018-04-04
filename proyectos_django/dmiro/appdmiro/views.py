162a5eaaeb180bbdd5f1b8b6a527bcb2af76ff06
jzevallos13 <37246131+jzevallos13@users.noreply.github.com>
2018-04-03 20:30:02 -0500

    Crear View fusionCircular3D Agencias
    Crear View fusionCircular3D Agencias, sera insertado en el template general.

--- proyectos_django/dmiro/appdmiro/views.py
+++ proyectos_django/dmiro/appdmiro/views.py
@@ -200,4 +200,43 @@ def fusionCircular(request,agencia,asesor,mes,anio):
             "data": data
 }
     return JsonResponse(responseData)	
-
+def fusionCircular3DAgencias(request,anio):
+    query_set = trasacciones.objects.filter(tra_anio=anio).values('id_agencias_id__age_nombre').annotate(suma=Sum('tra_valor')).order_by('-suma')
+    data = []   
+    for valores in query_set:
+        datos = {}
+        datos['label'] = valores['id_agencias_id__age_nombre']
+        datos['value'] = valores['suma']
+        data.append(datos)
+    responseData = {"chart": 
+    {
+                "caption": "Agencias en General",
+                "subCaption": "",
+                "numberPrefix": "$",
+                "paletteColors": "#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000",
+                "bgColor": "#ffffff",
+                "showBorder": "0",
+                "use3DLighting": "0",
+                "showShadow": "0",
+                "enableSmartLabels": "0",
+                "startingAngle": "310",
+                "showLabels": "0",
+                "showPercentValues": "1",
+                "showLegend": "1",
+                "legendShadow": "0",
+                "legendBorderAlpha": "0",                                
+                "decimals": "0",
+                "captionFontSize": "14",
+                "subcaptionFontSize": "14",
+                "subcaptionFontBold": "0",
+                "toolTipColor": "#ffffff",
+                "toolTipBorderThickness": "0",
+                "toolTipBgColor": "#000000",
+                "toolTipBgAlpha": "80",
+                "toolTipBorderRadius": "2",
+                "toolTipPadding": "5",
+            },
+            "data": data
+            }
+    
+    return JsonResponse(responseData)
