fb224674a8656e880a16f1b58099bcd7a3f12b85
gavilesv123 <glenn_rav@hotmail.com>
2018-04-04 00:40:18 -0500

    Se modfica el modelo se actualizan campos y tipos de datos
    @@ -25,29 +25,29 @@ class agencias(models.Model):
    		# Create your models here.
    
    class asesores(models.Model):
    	ase_nombres = models.CharField(max_length=100, blank=False, null=False)
    	ase_apellidos = models.CharField(max_length=100, blank=False, null=False)
    	ase_nombres =  models.CharField(max_length=100, blank=False, null=False)
    	ase_apellidos =  models.CharField(max_length=100, blank=False, null=False)
    	def __unicode__(self): #Python2
    		return self.ase_nombres
    	def __str__(self): #Python3
    		return self.ase_nombres
    
    class productos(models.Model):
    	pro_nombre = models.CharField(max_length=100, blank=False, null=False)
    	pro_nombre =  models.CharField(max_length=100, blank=False, null=False)
    	def __unicode__(self): #Python2
    		return self.pro_nombre
    	def __str__(self): #Python3
    		return self.pro_nombre
    
    class trasacciones(models.Model):
    	id_agencias = models.ForeignKey(agencias, on_delete=models.CASCADE)
    	id_asesores = models.ForeignKey(asesores, on_delete=models.CASCADE)
    	id_productos = models.ForeignKey(productos, on_delete=models.CASCADE)
    	tra_mes = models.CharField(max_length=2, blank=False, null=False)
    	tra_anio = models.CharField(max_length=4, blank=False, null=False)
    	tra_valor = models.PositiveIntegerField(validators=[MaxValueValidator(10)], blank=False, null=False)
    	id_agencias =  models.ForeignKey(agencias, on_delete=models.CASCADE)
    	id_asesores =  models.ForeignKey(asesores, on_delete=models.CASCADE)
    	id_productos =  models.ForeignKey(productos, on_delete=models.CASCADE)
    	tra_mes =  models.CharField(max_length=2, blank=False, null=False)
    	tra_anio =  models.CharField(max_length=4, blank=False, null=False)
    	tra_valor =  models.PositiveIntegerField(validators=[MaxValueValidator(10)], blank=False, null=False)
    
    	def __unicode__(self): #Python2
    		return self.tra_valor
    	def __str__(self): #Python3
    		return self.tra_valor
    		return self.tra_valor

--- proyectos_django/dmiro/appdmiro/models.py
+++ proyectos_django/dmiro/appdmiro/models.py
@@ -25,29 +25,29 @@ class agencias(models.Model):
 		# Create your models here.
 
 class asesores(models.Model):
-	ase_nombres = models.CharField(max_length=100, blank=False, null=False)
-	ase_apellidos = models.CharField(max_length=100, blank=False, null=False)
+	ase_nombres =  models.CharField(max_length=100, blank=False, null=False)
+	ase_apellidos =  models.CharField(max_length=100, blank=False, null=False)
 	def __unicode__(self): #Python2
 		return self.ase_nombres
 	def __str__(self): #Python3
 		return self.ase_nombres
 
 class productos(models.Model):
-	pro_nombre = models.CharField(max_length=100, blank=False, null=False)
+	pro_nombre =  models.CharField(max_length=100, blank=False, null=False)
 	def __unicode__(self): #Python2
 		return self.pro_nombre
 	def __str__(self): #Python3
 		return self.pro_nombre
 
 class trasacciones(models.Model):
-	id_agencias = models.ForeignKey(agencias, on_delete=models.CASCADE)
-	id_asesores = models.ForeignKey(asesores, on_delete=models.CASCADE)
-	id_productos = models.ForeignKey(productos, on_delete=models.CASCADE)
-	tra_mes = models.CharField(max_length=2, blank=False, null=False)
-	tra_anio = models.CharField(max_length=4, blank=False, null=False)
-	tra_valor = models.PositiveIntegerField(validators=[MaxValueValidator(10)], blank=False, null=False)
+	id_agencias =  models.ForeignKey(agencias, on_delete=models.CASCADE)
+	id_asesores =  models.ForeignKey(asesores, on_delete=models.CASCADE)
+	id_productos =  models.ForeignKey(productos, on_delete=models.CASCADE)
+	tra_mes =  models.CharField(max_length=2, blank=False, null=False)
+	tra_anio =  models.CharField(max_length=4, blank=False, null=False)
+	tra_valor =  models.PositiveIntegerField(validators=[MaxValueValidator(10)], blank=False, null=False)
 
 	def __unicode__(self): #Python2
 		return self.tra_valor
 	def __str__(self): #Python3
-		return self.tra_valor
+		return self.tra_valor 
